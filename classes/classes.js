d1dc174d5b8a9c37cd8eae510f6f800b5cf51459
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-01-31 11:23:16 -0500

    classes

---
+++ classes/classes.js
@@ -0,0 +1,24 @@
+class Human {
+    constructor() {
+        this.planet = 'Mars'
+    }
+
+    printPlanet() {
+        console.log(this.planet);
+    }
+}
+
+class Person extends Human {
+    constructor() {
+        super(); //calls the Human class constructor function
+        this.name = 'abe'
+    }
+
+    printName(){
+        console.log(this.name);
+    }
+}
+
+const person = new Person();
+person.printName();
+person.printPlanet();
